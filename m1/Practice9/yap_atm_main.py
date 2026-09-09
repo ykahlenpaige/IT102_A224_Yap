e4dd44acf0c8d5aead06c557cb04b5a747bc8583
ykahlenpaige <ykahlenpaige@gmail.com>
2026-09-09 11:33:06 +0000

    y

--- m1/Practice9/yap_atm_main.py
+++ m1/Practice9/yap_atm_main.py
@@ -303,3 +303,17 @@ elif choice == "Analyze Transactions":
         "Latest Activity",
         result["latest_timestamp"]
     )
+
+""" 
+######### Learning Signature ######### 
+Programmed by: Kahlen Yap
+Date Submitted: September 9, 2026
+ 
+Program Description: This program ____.
+Reflection: I learned ____.
+ 
+AI Usage
+[/] No AI Assistance – Completed independently without AI.
+[ ] AI as Support Tool – Used AI for explanations, syntax, or minor corrections.
+[ ] AI as Collaborative Partner﻿ – Used AI to design, structure, or co-create significant code.
+﻿"""
