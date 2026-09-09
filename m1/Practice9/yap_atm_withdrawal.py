c308094253712cbace46b628ef3ff62f232d2712
ykahlenpaige <ykahlenpaige@gmail.com>
2026-09-09 11:13:11 +0000

    y

---
+++ m1/Practice9/yap_atm_withdrawal.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+
+def withdraw_money(account, amount):
+
+    if amount <= 0:
+        return False
+
+    success = account.withdraw(amount)
+
+    if success:
+
+        timestamp = datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+
+        with open("transactions.txt", "a") as file:
+
+            file.write(
+                f"Timestamp: {timestamp}\n"
+            )
+
+            file.write(
+                f"Account: {account.account_name}\n"
+            )
+
+            file.write(
+                "Transaction: Withdraw\n"
+            )
+
+            file.write(
+                f"Amount: ₱{amount:.2f}\n\n"
+            )
+
+        return True
+
+    return False
