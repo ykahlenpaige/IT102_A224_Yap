ea459bd86c2940b9cf54f833aa9dd184ff7b217b
ykahlenpaige <ykahlenpaige@gmail.com>
2026-09-09 11:46:03 +0000

    y

--- m1/Practice9/yap_atm_analysis.py
+++ m1/Practice9/yap_atm_analysis.py
@@ -141,17 +141,3 @@ def analyze_transactions():
         "latest_timestamp": latest_timestamp,
         "largest_transaction": largest_transaction
     }
-
-    """ 
-######### Learning Signature ######### 
-Programmed by: Kahlen Yap
-Date Submitted: September 9, 2026
- 
-Program Description: This program ____.
-Reflection: I learned ____.
- 
-AI Usage
-[/] No AI Assistance – Completed independently without AI.
-[ ] AI as Support Tool – Used AI for explanations, syntax, or minor corrections.
-[ ] AI as Collaborative Partner﻿ – Used AI to design, structure, or co-create significant code.
-﻿"""
--- m1/Practice9/yap_atm_deposit.py
+++ m1/Practice9/yap_atm_deposit.py
@@ -35,17 +35,3 @@ def deposit_money(account, amount):
         return True
 
     return False
-
-""" 
-######### Learning Signature ######### 
-Programmed by: Kahlen Yap
-Date Submitted: September 9, 2026
- 
-Program Description: This program ____.
-Reflection: I learned ____.
- 
-AI Usage
-[/] No AI Assistance – Completed independently without AI.
-[ ] AI as Support Tool – Used AI for explanations, syntax, or minor corrections.
-[ ] AI as Collaborative Partner﻿ – Used AI to design, structure, or co-create significant code.
-﻿"""
